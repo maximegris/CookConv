d4418a05f1cb4b4718ef91192a5680f3e24dc99e
maximegris <maxime.gris@gmail.com>
2016-03-26 15:36:19 +0100

    Split config app in multi files

--- www/js/app.js
+++ www/js/app.js
@@ -7,13 +7,11 @@
   angular.module('services', ['constants', 'db.config']);
   angular.module('directives', ['constants', 'services']);
   angular.module('starter', ['ionic', 'ngCordova', 'pascalprecht.translate', 'directives', 'templates'])
-    .run(runApplication)
-    .config(ionicConfig)
-    .config(translateConfig)
-    .config(stateConfig);
+    .run(runApplication);
 
   runApplication.$inject = ['$ionicPlatform', '$cordovaSplashscreen'];
 
+  /* @ngInject */
   function runApplication($ionicPlatform, $cordovaSplashscreen) {
 
     $ionicPlatform.ready(function() {
@@ -46,143 +44,6 @@
       }
 
     });
-  }
-
-  ionicConfig.$inject = ['$ionicConfigProvider', '$logProvider', '$compileProvider'];
-
-  function ionicConfig($ionicConfigProvider, $logProvider, $compileProvider) {
-    if (ionic.Platform.isAndroid()) {
-      $logProvider.debugEnabled(false);
-      $compileProvider.debugInfoEnabled(false);
-    }
-
-    // Cache
-    $ionicConfigProvider.views.transition('none');
-    $ionicConfigProvider.views.maxCache(0);
-    $ionicConfigProvider.tabs.position('bottom');
-    $ionicConfigProvider.backButton.previousTitleText(false).text('');
-  }
-
-  translateConfig.$inject = ['$translateProvider'];
-
-  function translateConfig($translateProvider) {
-    $translateProvider
-      .useStaticFilesLoader({
-        prefix: 'assets/locales/',
-        suffix: '.json'
-      })
-      .registerAvailableLanguageKeys(['en', 'fr', 'de', 'es', 'pt', 'zh'], {
-        'en': 'en',
-        'en_*': 'en',
-        'EN_*': 'en',
-        'En_*': 'en',
-        'fr': 'fr',
-        'fr_*': 'fr',
-        'FR_*': 'fr',
-        'Fr_*': 'fr',
-        'de': 'de',
-        'de_*': 'de',
-        'DE_*': 'de',
-        'De_*': 'de',
-        'es': 'es',
-        'es_*': 'es',
-        'ES_*': 'es',
-        'Es_*': 'es',
-        'pt': 'pt',
-        'pt_*': 'pt',
-        'PT_*': 'pt',
-        'Pt_*': 'pt',
-        'zh': 'zh',
-        'zh_*': 'zh',
-        'ZH_*': 'zh',
-        'Zh_*': 'zh',
-      })
-      .determinePreferredLanguage()
-      .fallbackLanguage('en')
-      .useSanitizeValueStrategy('escapeParameters');
-  }
-
-  stateConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-
-  function stateConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-    // setup an abstract state for the tabs directive
-      .state('tab', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'tabs.html',
-      resolve: {
-        dbReady:dbReady
-      }
-    })
-
-    // Each tab has its own nav history stack:
-    .state('tab.calculator', {
-      url: '/calculator',
-      views: {
-        'tab-calculator': {
-          template: '<calculator></calculator>'
-        }
-      }
-    })
-
-    .state('tab.savings', {
-        url: '/savings',
-        views: {
-          'tab-savings': {
-            template: '<savings></savings>'
-          }
-        }
-      })
-      .state('tab.settings', {
-        url: '/settings',
-        views: {
-          'tab-settings': {
-            template: '<settings></settings>'
-          }
-        }
-      })
-      .state('tab.settings-lang', {
-
-        url: '/settings/lang',
-        views: {
-          'tab-settings': {
-            template: '<settings-lang><settings-lang>'
-          }
-        }
-
-      })
-      .state('tab.settings-ingredients', {
-
-        url: '/settings/ingredients',
-        views: {
-          'tab-settings': {
-            template: '<settings-ingredients><settings-ingredients>'
-          }
-        }
-      });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/calculator');
-
-    function dbReady(DBFactory, $log, $rootScope, $translate, $cordovaSplashscreen) {
-      // (1) init the DB
-      return DBFactory.initDB()
-        .then(function(success) {
-          return DBFactory.getContextApplication(success, $translate.use());
-        }, function(error) {
-          alert('Failed initDB : ' + JSON.stringify(error));
-        })
-        .then(function(success) {
-          $rootScope.init = true;
-          $rootScope.settings = success[0];
-          $rootScope.ingredients = success[1];
-          $rootScope.types = success[2];
-
-        }, function(error) {
-          alert('Failed getContextApplication: ' + JSON.stringify(error));
-        });
-    }
   }
 
 })(angular);
---
+++ www/js/config/app/app.routes.js
@@ -0,0 +1,91 @@
+(function(angular, undefined) {
+  'use strict';
+
+  angular.module('starter')
+    .config(stateConfig);
+
+  stateConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  /* @ngInject */
+  function stateConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'tabs.html',
+      resolve: {
+        dbReady: dbReady
+      }
+    })
+
+    // Each tab has its own nav history stack:
+    .state('tab.calculator', {
+      url: '/calculator',
+      views: {
+        'tab-calculator': {
+          template: '<calculator></calculator>'
+        }
+      }
+    })
+
+    .state('tab.savings', {
+        url: '/savings',
+        views: {
+          'tab-savings': {
+            template: '<savings></savings>'
+          }
+        }
+      })
+      .state('tab.settings', {
+        url: '/settings',
+        views: {
+          'tab-settings': {
+            template: '<settings></settings>'
+          }
+        }
+      })
+      .state('tab.settings-lang', {
+
+        url: '/settings/lang',
+        views: {
+          'tab-settings': {
+            template: '<settings-lang><settings-lang>'
+          }
+        }
+
+      })
+      .state('tab.settings-ingredients', {
+
+        url: '/settings/ingredients',
+        views: {
+          'tab-settings': {
+            template: '<settings-ingredients><settings-ingredients>'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/calculator');
+
+    function dbReady(DBFactory, $log, $rootScope, $translate, $cordovaSplashscreen) {
+      // (1) init the DB
+      return DBFactory.initDB()
+        .then(function(success) {
+          return DBFactory.getContextApplication(success, $translate.use());
+        }, function(error) {
+          alert('Failed initDB : ' + JSON.stringify(error));
+        })
+        .then(function(success) {
+          $rootScope.init = true;
+          $rootScope.settings = success[0];
+          $rootScope.ingredients = success[1];
+          $rootScope.types = success[2];
+
+        }, function(error) {
+          alert('Failed getContextApplication: ' + JSON.stringify(error));
+        });
+    }
+  }
+
+})(angular);
